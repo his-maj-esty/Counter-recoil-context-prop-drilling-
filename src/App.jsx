5ba53a6cfb2d45971fbdeca68b0410eef9f905bd
Brihat <brihats34@gmail.com>
2023-08-19 16:44:33 +0530

    Add basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex flex-col justify-around w-1/2 h-28 p-6  border-black border-2 rounded-md'>
+        <DisplayCounter count={count} setCount={setCount}></DisplayCounter>
+        <Buttons counter={count} setCount={setCount}></Buttons>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+
+function DisplayCounter({count, setCount}){
+  return (
+    <div>
+      {count}
+    </div>
+  );
+}
+
+function Buttons({count, setCount}){
+  return(
+    <div className='flex justify-between'>
+      <IncreaseButton count={count} setCount={setCount} ></IncreaseButton>
+      <DecreaseButton count={count} setCount={setCount}></DecreaseButton>
+    </div>
+  );
+}
+
+function IncreaseButton({count, setCount}){
+  return(
+  <button className='bg-red-500 p-1 rounded-sm text-white' onClick={() => setCount((c) => c+1)}>Increase</button>
+  );
+}
+
+function DecreaseButton({count, setCount}){
+  return(
+  <button className='bg-red-500 p-1 rounded-sm text-white' onClick={() => setCount((c) => c-1)}>decrease</button>
+  );
+}
+export default App;
