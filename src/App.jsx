9522c2c39737c6dbcded4871d982dd2b86871edd
Brihat <brihats34@gmail.com>
2023-08-19 19:17:39 +0530

    use Contexts in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import { useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import './App.css'
 
 // rerendering happens of component whenever its or its parent's state changes. buttons are getting rerendered because of App component
 // we want to reduce rerendering to only for DisplayCount.
+
+// context only solves prop drilling not rerendering because count is being updated in App which is parent to other elements
+const CountContext = createContext()
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='flex flex-col justify-around w-1/2 h-28 p-6  border-black border-2 rounded-md'>
-        <DisplayCounter count={count} setCount={setCount}></DisplayCounter>
-        <Buttons counter={count} setCount={setCount}></Buttons>
-      </div>
+      <CountContext.Provider value={{
+        count: count,
+        setCount: setCount
+      }}> 
+        <div className='flex flex-col justify-around w-1/2 h-28 p-6  border-black border-2 rounded-md'>
+          <DisplayCounter></DisplayCounter>
+          <Buttons></Buttons>
+        </div>
+      </CountContext.Provider>
     </>
   )
 }
 
 
-function DisplayCounter({count, setCount}){
+function DisplayCounter(){
+  const {count} = useContext(CountContext);
   return (
     <div>
       {count}
@@ -25,22 +34,24 @@ function DisplayCounter({count, setCount}){
   );
 }
 
-function Buttons({setCount}){
+function Buttons(){
   return(
     <div className='flex justify-between'>
-      <IncreaseButton setCount={setCount} ></IncreaseButton>
-      <DecreaseButton setCount={setCount}></DecreaseButton>
+      <IncreaseButton></IncreaseButton>
+      <DecreaseButton></DecreaseButton>
     </div>
   );
 }
 
-function IncreaseButton({setCount}){
+function IncreaseButton(){
+  const {setCount} = useContext(CountContext);
   return(
   <button className='bg-red-500 p-1 rounded-sm text-white' onClick={() => setCount((c) => c+1)}>Increase</button>
   );
 }
 
-function DecreaseButton({setCount}){
+function DecreaseButton(){
+  const {setCount} = useContext(CountContext);
   return(
   <button className='bg-red-500 p-1 rounded-sm text-white' onClick={() => setCount((c) => c-1)}>decrease</button>
   );
